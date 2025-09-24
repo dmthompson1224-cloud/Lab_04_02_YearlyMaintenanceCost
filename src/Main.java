cfa5437af031d16f60f22c5fec375dd82489f84b
dmthompson1224-cloud <dmthompson1224@gmail.com>
2025-09-24 14:54:16 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        double springCost = 100;
+        double summerCost = 200;
+        double fallCost = 150;
+        double winterCost = 300;
+
+        double totalyearlyCost = springCost + summerCost + fallCost + winterCost;
+        System.out.println("Total yearly cost: " + totalyearlyCost);
+    }
+}
